fd4dad895b4fdef29bcc234c8cebd2f8d43c7774
charlestytler <charles.tytler@gmail.com>
2017-12-19 21:06:05 -0500

    baro altimeter reading added

--- APM_Controller.ino
+++ APM_Controller.ino
@@ -7,17 +7,26 @@
 #include <AP_Param.h>
 #include <StorageManager.h>
 
+#include <AP_Baro.h>
+
+
 #define FRAME_TIME 10000    //microseconds
 #define MIN_TASK_TIME 2000  //microseconds
 
 // Identify which board type for Hardware Abstraction Layer (HAL)
 const AP_HAL::HAL& hal = AP_HAL_BOARD_DRIVER;
 
+// Identify sensors
+static AP_Baro_MS5611 barometer(&AP_Baro_MS5611::spi);
+
+static float _baro_alt;
 
 void setup(void){
   //Set baud rate to 57600 in order to transmit over SIK Radio
   hal.uartA->begin(map_baudrate(57600));
   
+  // Calibrate barometer
+  baro_calibration();
   // Calibrate accel
   // Calibrate gyro
   
@@ -25,7 +34,7 @@ void setup(void){
   
 }
 
-void loop(void){
+void loop(){
   
   // Get start time of frame
   uint32_t frame_start_time = hal.scheduler->micros();
@@ -35,15 +44,15 @@ void loop(void){
   
   do {
     // Check if at least MIN_TASK_TIME msec are left in frame to complete next task
-    if ( FRAME_TIME - (hal.scheduler->micros() - frame_start_time) >= MIN_TASK_TIME ) {
+    if ( FRAME_TIME - delta_time(frame_start_time) >= MIN_TASK_TIME ) {
       // Execute task
       task_count = execute_task(task_count, frame_start_time);
     }
   }while (task_count != 0);
   
   // Wait until FRAME_TIME is reached
-  while (hal.scheduler->micros() - frame_start_time < FRAME_TIME){
-    hal.scheduler->delay(1);
+  while (delta_time(frame_start_time) < FRAME_TIME){
+    //hal.scheduler->delay(1);
   }
   
 }
@@ -59,6 +68,7 @@ int execute_task(int task_count, uint32_t frame_start_time){
     
     case 1:
       // Read sensors
+      read_baro_alt();
       break;
     
     case 2:
@@ -87,9 +97,45 @@ int execute_task(int task_count, uint32_t frame_start_time){
 }
 
 
+uint32_t delta_time(uint32_t start_time){
+  uint32_t now = hal.scheduler->micros();
+  if (now > start_time){
+    return now - start_time;
+  }
+  else{
+    return 65535 - start_time + now;
+  }
+}
+
+void baro_calibration(){
+
+  hal.console->println("Calibrating barometer...\n");
+  hal.scheduler->delay(1000);
+
+
+  // disable CS on MPU6000
+  hal.gpio->pinMode(63, HAL_GPIO_OUTPUT);
+  hal.gpio->write(63, 1);
+
+
+  barometer.init();
+  barometer.calibrate();
+}
+
+void read_baro_alt(){
+
+  barometer.read();
+  _baro_alt = barometer.get_altitude();
+
+  if (!barometer.healthy()) {
+    hal.console->println("Baro not healthy");
+  }
+}
+
 void transmit_serial(uint32_t frame_start_time){
   // Print out how much time was used in this frame
-  hal.console->printf_P(PSTR("%d usec\n"), (int) hal.scheduler->micros() - frame_start_time);
+  hal.console->printf("%u usec", delta_time(frame_start_time));
+  hal.console->printf(" baro_alt=%f\n", _baro_alt);
 }
 
 AP_HAL_MAIN();
